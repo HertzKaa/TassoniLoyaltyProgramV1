b424c1bf37394ae7f1e55b45f8fece86aa8df89a
Wikipediq <claudio.cozzolino@studenti.unicam.it>
2023-10-10 18:48:56 +0200

    Classe Carta

--- src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/carta/Carta.java
+++ src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/carta/Carta.java
@@ -1,4 +1,51 @@
 package it.unicam.cs.ids.tassoniloyaltyplatform.carta;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+//import it.unicam.cs.ids.loyaltyplatform.cliente.Cliente;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@Entity(name = "Carta")
+@Table(name = "carta")
 public class Carta {
+    @Id
+    @Column(name = "carta_id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cartaId;
+    @OneToOne
+    @JsonBackReference
+    @JoinColumn(name = "cliente_id", referencedColumnName = "cliente_id")
+    private Cliente cliente;
+
+    public Carta(Long cartaId, Cliente cliente) {
+        this.cartaId = cartaId;
+        this.cliente = cliente;
+    }
+
+    public Carta(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Carta() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carta carta = (Carta) o;
+        return Objects.equals(getCartaId(), carta.getCartaId()) && Objects.equals(getCliente(), carta.getCliente());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCartaId(), getCliente());
+    }
 }
