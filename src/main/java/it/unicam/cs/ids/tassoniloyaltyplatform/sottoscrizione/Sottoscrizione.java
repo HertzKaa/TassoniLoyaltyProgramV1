94d037bfa43dd3dc188870ef29c848ac6829405a
Andrea Carlini <andrea01.carlini@studenti.unicam.it>
2023-10-20 13:18:43 +0200

    sottoscrizione update

--- src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/sottoscrizione/Sottoscrizione.java
+++ src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/sottoscrizione/Sottoscrizione.java
@@ -1,4 +1,16 @@
 package it.unicam.cs.ids.tassoniloyaltyplatform.sottoscrizione;
 
+import it.unicam.cs.ids.tassoniloyaltyplatform.cliente.Cliente;
+
 public class Sottoscrizione {
+
+    private Cliente cliente;
+
+    public Sottoscrizione(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
 }
