cf6d4fb172d848bf93688fd95795b4596694cfbc
andrea <andrea01.carlini@studenti.unicam.it>
2023-10-23 19:07:20 +0200

    ProgrammaFedelta Springboot inizio

--- src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/programmaFedelta/ProgrammaFedelta.java
+++ src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/programmaFedelta/ProgrammaFedelta.java
@@ -3,10 +3,7 @@ package it.unicam.cs.ids.tassoniloyaltyplatform.programmaFedelta;
 import it.unicam.cs.ids.tassoniloyaltyplatform.azienda.Azienda;
 import it.unicam.cs.ids.tassoniloyaltyplatform.cliente.Cliente;
 import it.unicam.cs.ids.tassoniloyaltyplatform.sottoscrizione.Sottoscrizione;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,9 +11,10 @@ import lombok.ToString;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-@Getter
-@Setter
+
 @ToString
+@Table(name = "programma_fedelta")
+@Entity(name = "programma_fedelta")
 public class ProgrammaFedelta {
     @Id
     @Column(name="programma_fedelta_id", nullable=false, updatable= false)
@@ -28,18 +26,18 @@ public class ProgrammaFedelta {
     @Column(name= "nome", nullable = false, columnDefinition = "TEXT")
     private String nomeProgramma;
 
-    private Azienda azienda;
+    //private Azienda azienda;
 
     @Column(name= "numero_clienti", nullable = false, columnDefinition = "NUM")
     private int numClienti;
 
-    private List<Sottoscrizione> sottoscrizioni;
+    //private List<Sottoscrizione> sottoscrizioni;
 
     public ProgrammaFedelta(Long programmaFedeltaId, int tipo, String nomeProgramma, Azienda azienda, int numClienti, List<Sottoscrizione> sottoscrizioni) {
         this.programmaFedeltaId = programmaFedeltaId;
         this.tipo = tipo;
         this.nomeProgramma = nomeProgramma;
-        this.azienda = azienda;
+        //this.azienda = azienda;
         this.numClienti = numClienti;
         sottoscrizioni= new ArrayList<>();
     }
@@ -47,9 +45,13 @@ public class ProgrammaFedelta {
     public ProgrammaFedelta(int tipo, String nomeProgramma, Azienda azienda, int numClienti) {
         this.tipo = tipo;
         this.nomeProgramma = nomeProgramma;
-        this.azienda = azienda;
+        //this.azienda = azienda;
         this.numClienti = numClienti;
-        sottoscrizioni= new ArrayList<>();
+        //sottoscrizioni= new ArrayList<>();
+    }
+
+    public ProgrammaFedelta() {
+
     }
 
     @Override
