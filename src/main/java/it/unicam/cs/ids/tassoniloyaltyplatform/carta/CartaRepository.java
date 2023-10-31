b2ae2199bc6e06db74c40b61818fbd34e1dde63b
andrea <andrea01.carlini@studenti.unicam.it>
2023-10-31 10:45:00 +0100

    ClienteRepository

--- src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/carta/CartaRepository.java
+++ src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/carta/CartaRepository.java
@@ -4,7 +4,12 @@ import it.unicam.cs.ids.tassoniloyaltyplatform.azienda.Azienda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CartaRepository extends JpaRepository<>{
+import java.util.Optional;
 
+@Repository
+public interface CartaRepository extends JpaRepository<Carta,Long>{
+
+
+    @Override
+    Optional<Carta> findById(Long aLong);
 }
