9df1f1caf72bf520f2598037b12b2794f64ac793
andrea <andrea01.carlini@studenti.unicam.it>
2023-11-06 16:45:50 +0100

    Carta

--- src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/carta/CartaRepository.java
+++ src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/carta/CartaRepository.java
@@ -1,6 +1,7 @@
 package it.unicam.cs.ids.tassoniloyaltyplatform.carta;
 
 import it.unicam.cs.ids.tassoniloyaltyplatform.azienda.Azienda;
+import it.unicam.cs.ids.tassoniloyaltyplatform.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,7 @@ public interface CartaRepository extends JpaRepository<Carta,Long>{
 
 
     @Override
-    Optional<Carta> findById(Long aLong);
+    Optional<Carta> findCartaByCliente(Cliente cliente);
+
+
 }
--- src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/carta/CartaService.java
+++ src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/carta/CartaService.java
@@ -1,12 +1,21 @@
 package it.unicam.cs.ids.tassoniloyaltyplatform.carta;
 
+import it.unicam.cs.ids.tassoniloyaltyplatform.cliente.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Service
 public class CartaService {
     private final CartaRepository cartaRepository;
-
+    @Autowired
     public CartaService(CartaRepository cartaRepository) {
         this.cartaRepository = cartaRepository;
+    }
+    @GetMapping
+    public Optional<Carta> getCartaByCliente(Cliente cliente) {
+        return cartaRepository.findCartaByCliente(cliente);
     }
 }
