2b7558bb46e603b6836386ef0607bc1978a0b772
andrea <andrea01.carlini@studenti.unicam.it>
2023-11-30 17:48:56 +0100

    PremioService, PremioRepository

--- src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/premio/PremioService.java
+++ src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/premio/PremioService.java
@@ -1,18 +1,46 @@
 package it.unicam.cs.ids.tassoniloyaltyplatform.premio;
 
 import it.unicam.cs.ids.tassoniloyaltyplatform.dto.premioLivelloDTO;
+import it.unicam.cs.ids.tassoniloyaltyplatform.exception.ResourceAlreadyExistsException;
+import it.unicam.cs.ids.tassoniloyaltyplatform.exception.ResourceNotFoundException;
+import it.unicam.cs.ids.tassoniloyaltyplatform.livello.Livello;
+import it.unicam.cs.ids.tassoniloyaltyplatform.livello.LivelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PremioService {
-    public Premio findPremioById(Long id) {
+    private final PremioRepository premioRepository;
+    private final LivelloService livelloService;
+    @Autowired
+    public PremioService(PremioRepository premioRepository, LivelloService livelloService) {
+        this.premioRepository = premioRepository;
+        this.livelloService = livelloService;
     }
-
+    @GetMapping
+    public Premio findPremioById(Long id)throws ResourceNotFoundException {
+        Optional<Premio> premio = premioRepository.findById(id);
+        if(premio.isPresent()) return  premio.get();
+        else  throw new ResourceNotFoundException();
+    }
+    @GetMapping
     public List<Premio> getAllPremi() {
+        return this.premioRepository.findAll();
     }
+    @GetMapping
+    public void aggiungiPremioLivello(premioLivelloDTO premioDTO)throws ResourceNotFoundException, ResourceAlreadyExistsException {
+        Livello livello = this.livelloService.findLivelloByID(premioDTO.getLivelloId());
+        for (Premio p: livello.getCatalogoPremi()){
+            if(p.getNome().equals(premioDTO.getNome())){
+                throw new ResourceAlreadyExistsException();
+            }
 
-    public void aggiungiPremioLivello(premioLivelloDTO premioDTO) {
+        }
+        Premio premio = new Premio(livello, premioDTO.getNome(), premioDTO.getDescrizione());
+        this.livelloService.aggiungiPremio(livello,premio);
     }
 }
