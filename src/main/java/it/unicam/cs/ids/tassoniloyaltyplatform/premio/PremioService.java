f99a790268c5167e6941959353f806c257739848
andrea <andrea01.carlini@studenti.unicam.it>
2023-11-30 17:34:49 +0100

    PremioController

--- src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/premio/PremioService.java
+++ src/main/java/it/unicam/cs/ids/tassoniloyaltyplatform/premio/PremioService.java
@@ -1,4 +1,18 @@
 package it.unicam.cs.ids.tassoniloyaltyplatform.premio;
 
+import it.unicam.cs.ids.tassoniloyaltyplatform.dto.premioLivelloDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PremioService {
+    public Premio findPremioById(Long id) {
+    }
+
+    public List<Premio> getAllPremi() {
+    }
+
+    public void aggiungiPremioLivello(premioLivelloDTO premioDTO) {
+    }
 }
